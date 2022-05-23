b33822637e59b8fac0ef799e90df4d02c7ea9b6a
Eduards-Belinskis <e.belinskis@gmail.com>
2022-05-23 04:26:06 +0300

    Update FirstClass.java

--- src/main/java/homework/FirstClass.java
+++ src/main/java/homework/FirstClass.java
@@ -4,5 +4,23 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("This is homework");
+
+        String valstsNosaukums = "Latvija";
+        int population = 1902000;
+        double area = 64589.6;
+        String capital = "Riga";
+        String officialLanguage = "Latvian";
+        boolean isEU = true;
+        char currency = '€';
+
+        System.out.println("Valsts nosaukums ir " + valstsNosaukums + " un tās iedzīvotāju skaits ir " + population + "cilvēku, tās " +
+                "galvas pilsēta ir " + capital + "kā arī oficiālā valsts valoda ir " + officialLanguage);
+
+        System.out.println("Vai LV ir EU savienībā? " + isEU);
+
+        System.out.println("Latvijas teritorija ir " + area + "km2 liela, un tās valūta ir " + currency + ".");
+
+        
+
     }
 }
